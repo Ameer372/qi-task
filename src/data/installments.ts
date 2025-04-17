ad54fa0e86b8e1683a6e2bd039d9e957078a24cb
Ameer Salim <ameersalim80@gmail.com>
2025-04-17 13:13:03 +0300

    Add dummy data

---
+++ src/data/installments.ts
@@ -0,0 +1,23 @@
+export const installments = [
+  {
+    id: "inst1",
+    orderId: "o1",
+    amount: 150,
+    dueDate: "2025-01-01",
+    status: "unpaid",
+  },
+  {
+    id: "inst2",
+    orderId: "o1",
+    amount: 150,
+    dueDate: "2025-02-01",
+    status: "unpaid",
+  },
+  {
+    id: "inst3",
+    orderId: "o2",
+    amount: 210,
+    dueDate: "2025-01-15",
+    status: "paid",
+  },
+];
---
+++ src/data/items.ts
@@ -0,0 +1,14 @@
+export const items = [
+  {
+    id: "i1",
+    name: 'Samsung TV 55"',
+    category: "Electronics",
+    price: 450,
+  },
+  {
+    id: "i2",
+    name: "Men’s Jacket",
+    category: "Clothing",
+    price: 210,
+  },
+];
