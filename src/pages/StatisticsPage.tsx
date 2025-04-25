cb6f119b8cb6920b2e318e82181a36d401e8e2e5
Ameer Salim <ameersalim80@gmail.com>
2025-04-25 18:39:37 +0300

    Refactor: removed unnecessary code

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -1,13 +1,10 @@
 import StatisticsCard from "@/components/StatisticsCard";
-import { useIsMobile } from "@/hooks/use-mobile";
 import { Link } from "wouter";
 import useStatistics from "@/hooks/useStatistics";
 import { useTranslation } from "react-i18next";
 import OrdersTable from "@/components/orders/OrdersTable";
 
 const StatisticsPage = () => {
-  const isMobile = useIsMobile();
-
   const { t } = useTranslation();
 
   const { stats, merchants, orders, isLoading, hasError } = useStatistics();
@@ -15,46 +12,6 @@ const StatisticsPage = () => {
   if (isLoading) return <div>{t("loading")}</div>;
   if (hasError || !orders || !merchants) return <div>Something went wrong</div>;
 
-  {
-    /*Mobile*/
-  }
-  if (isMobile)
-    return (
-      <div className="p-6">
-        <h1>{t("statistics")}</h1>
-        <div className="p-6 flex gap-6 md:flex-row flex-col">
-          {stats.map((stat) =>
-            stat.href ? (
-              <Link href={stat.href} key={stat.title} className={"flex-1"}>
-                <StatisticsCard
-                  title={stat.title}
-                  content={stat.count}
-                  icon={
-                    <stat.icon className="fill-yellow-300 dark:fill-yellow-500" />
-                  }
-                ></StatisticsCard>
-              </Link>
-            ) : (
-              <StatisticsCard
-                key={stat.title}
-                title={stat.title}
-                content={stat.count}
-                icon={<stat.icon />}
-              ></StatisticsCard>
-            )
-          )}
-        </div>
-        {/*Last Orders Table*/}
-        <div className="p-6">
-          <h1 className="text-2xl font-semibold mb-4">{t("recent_orders")}</h1>
-          <OrdersTable orders={orders} merchants={merchants} />
-        </div>
-      </div>
-    );
-
-  {
-    /*Desktop*/
-  }
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-4">{t("statistics")}</h1>
